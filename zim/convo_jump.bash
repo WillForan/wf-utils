23a4ea59019bad7aabf030c70dcb4808d80ec8c3
WillForan <willforan@gmail.com>
2018-10-08 16:49:16 -0400

    convo jump works in terminal in addition to pidgin

--- zim/convo_jump.bash
+++ zim/convo_jump.bash
@@ -7,10 +7,28 @@ source $(dirname $0)/quickjump_src.bash
 read notebookdir notebookname <<< $(getnotebook $1)
 findinnotebook() { rg -i -l $2 -g '!Calendar' $1 | sed "s:$1::g"; }
 
-person="@$(xdotool getactivewindow getwindowname)"
-zimpath=$(findinnotebook $notebookdir $person | uritopath | menu )
+class="$(xprop -id $(xdotool getactivewindow ) WM_CLASS)"
+case "$class" in
+   *Pidgin*)
+      person="@$(xdotool getactivewindow getwindowname)"
+      zimpath=$(findinnotebook $notebookdir $person | uritopath | menu )
+      noi3jump=""
+      ;;
+   *term*|*urxvt*)
+      directory="$(xdotool getactivewindow getwindowname|cut -f2 -d:|
+          perl -F/ -slane 'print join "/", @F[0..$#F-$_] for (0..$#F-1)'|menu)"
+      [ -z "$directory" ] && exit
+      zimpath=$(findinnotebook $notebookdir $directory | uritopath | menu )
+      echo "set no i3jump"
+      noi3jump="nojump"
+      ;;
+   *)
+     echo "unknown class $class" >&2;
+     exit 1
+     ;;
+esac
 [ -z "$zimpath" ] && exit # escaped menu, nothing to do
 
 filepath=$notebookdir/$(echo $zimpath | pathtouri )
 [ ! -r "$filepath" ] && echo "$zimpath DNE as $filepath!?" && exit 1
-gotozim $zimpath $notebookname $filepath
+gotozim $zimpath $notebookname $filepath $noi3jump
